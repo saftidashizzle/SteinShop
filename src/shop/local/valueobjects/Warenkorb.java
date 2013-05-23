2ea222e0d7d94df13ea76799ea6cbd8ed25c3537
Jan Hinz <smartgit@enteente.de>
2013-05-23 09:41:17 +0200

    sorry <3

---
+++ src/shop/local/valueobjects/Warenkorb.java
@@ -0,0 +1,25 @@
+package shop.local.valueobjects;
+
+import java.util.HashMap;
+
+public class Warenkorb {
+
+	private HashMap<Artikel, Integer> warenkorb = new HashMap<Artikel, Integer>();
+
+	public void artikelHinzufuegen(Artikel a, int menge) {
+		if (warenkorb.containsKey(a)) {
+			int alteMenge = warenkorb.get(a);
+			warenkorb.put(a, alteMenge + menge);
+		} else {
+			warenkorb.put(a, menge);
+		}
+	}
+	
+	public void artikelEntfernen(Artikel a) {
+		
+	}
+	
+	public void leeren() {
+		warenkorb.clear();
+	}
+}

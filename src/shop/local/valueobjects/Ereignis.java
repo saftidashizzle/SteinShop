2e08c005da2cc7a67b4fccfc386240c8677681bb
Philipp Marsch <pmarsch@stud.hs-bremen.de>
2013-05-27 22:41:39 +0200

    Shop UI optimiert, Ereignisloggen erweitert und Methode fuegeArtikelEin mit Mengen-Parameter entfernt (wurde dann automatisch mit Menge 1 erstellt), da wir dass später in der GUI auch so lösen können, dass man nur 1<x erlauben.

--- src/shop/local/valueobjects/Ereignis.java
+++ src/shop/local/valueobjects/Ereignis.java
@@ -4,26 +4,16 @@ public class Ereignis {
 	private User akteur;
 	private int jahrestag;
 	private Artikel artikel;
-	private int anzahl;
 	private String aktion;
 	
-//	public Ereignis(String titel, int menge) {
-//	// nur noch hinkriegen das ich aus den uebergebenen parametern die attribute einstelle und gut ist.	
-//	// optimal: User akteur, int jahrestag, Artikel artikel, int anzahl	
-//	}
-//	public Ereignis(int nummer, int anzahl) {
-//		
-//	}
 	public Ereignis(User akteur, int jahrestag, Artikel artikel, int anzahl	, String aktion) {
 		this.akteur = akteur;
 		this.jahrestag = jahrestag;
 		this.artikel = artikel;
-		this.anzahl = anzahl;
+		this.artikel.setMenge(anzahl);
 		this.aktion = aktion;
 	}
 	public String toString() {
-		return ("User: " + akteur + " | Jahrestag: " + jahrestag + " | Artikel: " + artikel + " | Anzahländerung: " + anzahl + " | Aktion: " + aktion);
-//		return ("User Nr: " + akteur.getNummer() + "| User Name: " + akteur.getName() + "| Jahrestag: " + jahrestag + "| Artikel Nr: " + artikel.getNummer() + " | Artikel Name: " + artikel.getName() + " | Anzahl: " + anzahl);
-		
+		return ("Datum: " + jahrestag + " User: " + akteur + " Artikel: " + artikel + " Aktion: " + aktion);
 	}
 }

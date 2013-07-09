a19221bfa19e85f780798c4230c595564e6a69f6
Philipp Marsch <pmarsch@stud.hs-bremen.de>
2013-07-09 02:04:32 +0200

    topbild hinzugefuegt

--- client/gui/TopPanel.java
+++ client/gui/TopPanel.java
@@ -1,5 +1,6 @@
 package gui;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -11,6 +12,6 @@ public class TopPanel extends JPanel {
 
 	public TopPanel() {
 		super();
-		this.add(new JLabel("Stein Shop: Hallo!"));
+		this.add(new JLabel(new ImageIcon("Bild.png")));
 	}
 }

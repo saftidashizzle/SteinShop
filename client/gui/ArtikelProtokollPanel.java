9f70d0bd7ddd1af58335d719cb6d62b83f82c084
Philipp Marsch <pmarsch@stud.hs-bremen.de>
2013-07-09 09:59:46 +0200

    login panel vorausgefüllte sachen fehlen und ueberlegungen für graphen hinzugefuegt

--- client/gui/ArtikelProtokollPanel.java
+++ client/gui/ArtikelProtokollPanel.java
@@ -36,24 +36,48 @@ public class ArtikelProtokollPanel extends JPanel {
 		Calendar heute = Calendar.getInstance();
 		for(Ereignis e:artikelVerlauf) {
 			// in ms
-//			xyseries.add(e.getDate().getTime(), e.getArtikel().getMenge());
+			// xyseries.add(e.getDate().getTime(), e.getArtikel().getMenge());
 			long zeitVergangen = e.getDate().getTime() - heute.getTime().getTime();  // Differenz in ms
 			long inTagen = Math.round( (double)zeitVergangen / (24. * 60.*60.*1000.) ); // Zeit Differenz in Tagen
 			xyseries.add(inTagen, e.getArtikel().getMenge());
 		}
+		
+//		for(int i=0;i<30;i++) {
+//			// ereignis passend zu dem tag heraussuchen falls es ein gibt
+//			Ereignis e = 
+			// menge an dem tag heraussuchen
+//			xyseries.add(i, e.getArtikel().getMenge());
+//		}
+		
+		
+		
 	    XYSeriesCollection xyseriescollection = new XYSeriesCollection();   
 	    xyseriescollection.addSeries(xyseries);    
 	    xyseriescollection.setIntervalWidth(0.0D);   
 	    return xyseriescollection;   
 	} 
     public void drawChart(List<Ereignis> artikelVerlauf, String artikelName) {
-//    	Function2D normal = new NormalDistributionFunction2D(0.0, 1.0);
+    	// laeuft
         XYDataset dataset = createDataset(artikelVerlauf, artikelName);  
 		final JFreeChart chart= ChartFactory.createXYLineChart(null, "Zeitpunkt", "Lagerbestand", dataset, PlotOrientation.VERTICAL, true, true, false);
-//		chart.setBackgroundImage(image);
 		final ChartPanel chartPanel = new ChartPanel(chart);
 		chartPanel.setPreferredSize(new java.awt.Dimension(500,270));
 		this.removeAll();
 		this.add(chartPanel);
+		
+//        XYDataset dataset = createDataset(artikelVerlauf, artikelName);  
+//		JFreeChart chart= ChartFactory.createXYLineChart(null, "Zeitpunkt", "Lagerbestand", dataset, PlotOrientation.VERTICAL, true, true, false);
+//		
+//		XYPlot plot = (XYPlot) chart.getPlot();
+//		XYSplineRenderer renderer = new XYSplineRenderer();
+//		
+//        plot.setRenderer(renderer);
+//		
+//		final ChartPanel chartPanel = new ChartPanel(chart);
+//		chartPanel.setPreferredSize(new java.awt.Dimension(500,270));
+//		this.removeAll();
+//		this.add(chartPanel);
+
+        
     }
 }
